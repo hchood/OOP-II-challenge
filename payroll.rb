bd3828a744f5dff5e90d76ce8c933a6c4bdeeddd
Helen Hood <helen.c.hood@gmail.com>
2013-12-03 17:54:13 -0500

    cleaned up some stuff

--- payroll.rb
+++ payroll.rb
@@ -7,39 +7,15 @@ require 'pry'
 
 require_relative 'employee'
 require_relative 'sale'
-
-class Company
-  attr_reader :monthly_quota, :sales
-
-  def initialize(monthly_quota)
-    @monthly_quota = monthly_quota.to_i
-    @sales = 0
-  end
-
-  def import_sales
-    @sales = Sale.gross_sales
-  end
-
-  def hit_quota?
-    @sales >= @monthly_quota
-  end
-end
+require_relative 'company'
 
 # ********************************************************************************************
 #                                   PROGRAM
 # ********************************************************************************************
 
-# Your job is to write a program that calculates everyone's net pay.
-#Assume that payroll is disbursed monthly, so you can divide annual salaries by twelve to determine gross base pay.
-#Trailing decimals are rounded down to the lowest penny.
-
-# So that the system is easy to update, we recommend you create a CSV of each employee so it is easy to add and remove employees from the system.
-
-
 Employee.parse_employees('employees.csv')
 Sale.parse_sales('sales.csv')
 
-# binding.pry
 employees = Employee.employees
 
 employees.each do |employee|
