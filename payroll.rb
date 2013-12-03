4e2ea5b38e41bf955c98507a0bd67669b4f682a5
Helen Hood <helen.c.hood@gmail.com>
2013-12-03 17:50:50 -0500

    fixed output to match Apollo output.  I think I'm done

--- payroll.rb
+++ payroll.rb
@@ -45,6 +45,7 @@ employees = Employee.employees
 employees.each do |employee|
   puts "***#{employee.name}***"
   puts "Gross Salary: $#{sprintf('%.2f', employee.gross_monthly_salary)}"
+  puts "Commission: $#{sprintf('%.2f', employee.commission)}" unless employee.class == Employee
   puts "Net Pay: $#{sprintf('%.2f', employee.net_pay)}"
   puts "***\n\n"
 end
