1fefdaa415a7690095273a112481bf2180cc4c5c
Helen Hood <helen.c.hood@gmail.com>
2013-12-03 17:42:45 -0500

    working version, but need to fix gross_monthly_salary & net_pay methods, and define commission method and fix output for salespeople

--- employee.rb
+++ employee.rb
@@ -1,12 +1,14 @@
 class Employee
   attr_reader :name, :last_name, :base_salary
 
+  @@employees = []
+
   TAX_RATE = 0.30
 
   def initialize(name, base_salary)
     @name = name
     @last_name = name.split(/ /).last
-    @base_salary = base_salary.to_i
+    @base_salary = base_salary.to_f
   end
 
   def self.employees
@@ -14,7 +16,6 @@ class Employee
   end
 
   def self.parse_employees(filename)
-    @@employees = []
     CSV.foreach(filename, headers: true) do |row|
       if row["type"] == "1"
         @@employees << Employee.new(row["name"], row["base_salary"])
@@ -40,6 +41,8 @@ class Employee
 end
 
 class CommissionSalesPerson < Employee
+  attr_reader :sales, :commission
+
   def initialize(name, base_salary, commission_percentage)
     super(name, base_salary)
     @commission = commission_percentage.to_f
@@ -56,6 +59,8 @@ class CommissionSalesPerson < Employee
 end
 
 class QuotaSalesPerson < Employee
+  attr_reader :sales
+
   def initialize(name, base_salary, quota_bonus, quota)
     super(name, base_salary)
     @quota_bonus = quota_bonus.to_i
